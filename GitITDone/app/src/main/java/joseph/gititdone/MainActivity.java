f96cba1ace475f9907dee9535882ff13698a2248
Joseph Nguyen <jnguy019@fiu.edu>
2018-02-03 14:46:14 -0500

    added functionality for radioButtons

--- GitITDone/app/src/main/java/joseph/gititdone/MainActivity.java
+++ GitITDone/app/src/main/java/joseph/gititdone/MainActivity.java
@@ -5,19 +5,34 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView by, in;
-
+    Button doneBut;
+    RadioButton radBut;
+    RadioGroup radGroup;
+    String radButString;
+    int radioButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        radGroup = (RadioGroup) findViewById(R.id.toggle);
+        radioButton = radGroup.getCheckedRadioButtonId();
 
+    }
+    public void doneClick(View v) {
+        radioButton = radGroup.getCheckedRadioButtonId();
+        radBut = (RadioButton) findViewById(radioButton);
+        radButString = radBut.getText().toString();
     }
 
 }
