e449c2fd8752f82c7553be07801a656fda6994e0
Joseph Nguyen <jnguy019@fiu.edu>
2018-02-03 15:49:18 -0500

    Whole Project

--- GitITDone/GitITDone/app/src/main/java/joseph/gititdone/MainActivity.java
+++ GitITDone/GitITDone/app/src/main/java/joseph/gititdone/MainActivity.java
@@ -1,23 +1,21 @@
 package joseph.gititdone;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button doneBut;
     RadioButton radBut;
     RadioGroup radGroup;
-    String radButString;
+    String radButString, task, time;
     int radioButton;
+    //EditText task, time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         radioButton = radGroup.getCheckedRadioButtonId();
         radBut = (RadioButton) findViewById(radioButton);
         radButString = radBut.getText().toString();
+        task = 
+
     }
 
 }
