325aac451989e225e6466b037fae12db6c16934c
Joseph Nguyen <jnguy019@fiu.edu>
2018-02-03 12:59:23 -0500

    inital commit

---
+++ GitITDone/app/src/main/java/joseph/gititdone/MainActivity.java
@@ -0,0 +1,23 @@
+package joseph.gititdone;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView by, in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
